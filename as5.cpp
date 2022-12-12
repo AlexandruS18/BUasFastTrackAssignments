c5e3de16f87a6fe4d45fa2707ed2578ec1e1e652
Alex <116825414+niconicoenjoyer@users.noreply.github.com>
2022-12-12 23:24:19 +0200

    Update as5.cpp

--- as5.cpp
+++ as5.cpp
@@ -1,73 +1,77 @@
 #include "game.h"
 #include "surface.h"
-#include <cstdint>
-#include <iostream>
+// #include <iostream>
+
+int speedDamp(int spd)
+{
+    return -(spd - 2);
+}
 
 namespace Tmpl8
 {
-    // -----------------------------------------------------------
-    // Initialize the application
-    // -----------------------------------------------------------
     void Game::Init()
     {
+
     }
 
     void Game::Shutdown()
     {
     }
 
-    static Sprite theSprite(new Surface("assets/ball.png"), 1);
+    constexpr int ScreenWidth = 800;
+    constexpr int ScreenHeight = 512;
+
+    Sprite theSprite(new Surface("assets/ball.png"), 1);
     static int frame = 0;
+
     int spriteX = 0;
     int spriteY = 100;
-    int speedX = 1;
-    int speedY = 1;
+    int speedX = 3;
+    int speedY = 4;
     int hitTimes = 0;
 
-    // Main tick function
     void Game::Tick(float deltaTime)
     {
-        // clear the graphics window
         screen->Clear(0);
+        
         spriteX += speedX;
-        speedX++;
+        speedX += 4;
         spriteY += speedY;
-        speedY++;
+        speedY += 3;
 
-        bool hitTop = (spriteY < 0 + 50);
-        bool hitSideR = (spriteX > 800 - 50);
-        bool hitSideL = (spriteX < 0 + 50);
-        bool hitBottom = spriteY > 512 - 50;
-
-        if (hitTop)
-        {
-            spriteY = 0 + 50;
-            speedY = -speedY;
-        }
-
-        if (hitSideL)
-        {
-            spriteX = 0 + 50;
-            speedX = -speedX;
-        }
-        else if (hitSideR)
-        {
-            spriteX--;
-            speedX = -speedX;
-        }
+        bool hitBottom = spriteY > ScreenHeight - theSprite.GetHeight();
+        bool hitTop = spriteY < 0;
+        bool hitSide = spriteX > ScreenWidth - theSprite.GetWidth() || spriteX < 0;
 
         if (hitBottom)
         {
             hitTimes++;
-            if (hitTimes >= 50)
-            {
+            if (hitTimes == 50)
                 return;
-            }
-            spriteY = 512 - 50;
-            speedY = -speedY;
-            // std::cout << hitTimes << "\n";
+
+            spriteY = ScreenHeight - theSprite.GetHeight();
+            speedY = speedDamp(speedY);
         }
 
+        if (hitTop)
+        {
+            spriteY = 0;
+            speedY = speedDamp(speedY);
+        }
+
+        if (hitSide)
+        {
+            if (spriteX < 0)
+            {
+                spriteX = 0 + theSprite.GetWidth();
+                speedX = speedDamp(speedX);
+            }
+            else
+            {
+                spriteX = ScreenWidth - theSprite.GetWidth();
+                speedX = speedDamp(speedX);
+            }
+        }
         theSprite.Draw(screen, spriteX, spriteY);
     }
-}
+};
