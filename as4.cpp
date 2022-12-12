c10b788b268b30dc8e78366b69e16f76d6bcd1fd
Alex <116825414+niconicoenjoyer@users.noreply.github.com>
2022-12-12 23:26:01 +0200

    Update as4.cpp

--- as4.cpp
+++ as4.cpp
@@ -24,19 +24,6 @@ namespace Tmpl8
 	static int frame = 0;
 	int x = 0, y = 1;
 
-	void Game::DrawI(int x, int y)
-	{
-		screen->Line(100 + x, 50 + y, 200 + x, 50 + y, 0xffffff);
-		screen->Line(150 + x, 50 + y, 150 + x, 300 + y, 0xffffff);
-		screen->Line(100 + x, 300 + y, 200 + x, 300 + y, 0xffffff);
-	}
-	void Game::DrawFatI(int x, int y)
-	{
-		DrawI(x, y);
-		DrawI(x + 1, y);
-		DrawI(x, y + 1);
-		DrawI(x + 1, y + 1);
-	}
 	// -----------------------------------------------------------
 	// Main application tick function
 	// -----------------------------------------------------------
@@ -45,14 +32,18 @@ namespace Tmpl8
 
 		// clear the graphics window
 		screen->Clear(0);
-		for (int i = 0; i < 16; i++)
-		{
-			theSprite.SetFrame(i);
-			for (int j = 0; j < 10; j++)
-			{
-				theSprite.Draw(screen, i * 50, j * 50);
-			}
-		}
+	void Game::Tick(float deltaTime)
+   	{
 
+		// clear the graphics window
+		screen->Clear(0);
+		for (int i = 0; i < 10; i++)
+		{
+		    for (int j = 0; j < 16; j++)
+		    {
+			theSprite.SetFrame(j);
+			theSprite.Draw(screen, i * 50, j * 50);
+		    }
+		}
 	}
 };
